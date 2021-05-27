9fd76c2145434c7aca50b831cebb020a0f44a370
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2021-05-27 15:09:24 +0200

    change env.template.js

--- docker/config/env.template.js
+++ docker/config/env.template.js
@@ -19,7 +19,15 @@
     "molgenisPassword": "${MOLGENIS_PASSWORD}"
   };
 
-  window["env"]["mdrConfig"] = {
+  window["env"]["mdrConfigGBN"] = {
+    "mdrRestUrl": "${MDR_API_URL}",
+    "mdrNamespace": "${MDR_NAMESPACE}",
+    "languageCode": "${MDR_LANGUAGE}",
+    "dataElementGroups": "${MDR_MAPPING}",
+    "dataElements": "${MDR_FIELD_PROPERTIES}",
+    "hiddenDataElements": "${MDR_HIDDEN}"
+  };
+  window["env"]["mdrConfigCCDG"] = {
     "mdrRestUrl": "${MDR_API_URL}",
     "mdrNamespace": "${MDR_NAMESPACE}",
     "languageCode": "${MDR_LANGUAGE}",
